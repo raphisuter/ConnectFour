abb2bbd4791aac5ea562d7af77d6ffd280c1a6aa
xestho <raphael.suter@stud.hslu.ch>
2014-05-06 11:16:48 +0200

    Testclass

---
+++ ConnectFour/src/connectfour/Test.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package connectfour;
+
+/**
+ *
+ * @author Suter Raphael <raphael.suter@stud.hslu.ch>
+ */
+public class Test {
+
+}
