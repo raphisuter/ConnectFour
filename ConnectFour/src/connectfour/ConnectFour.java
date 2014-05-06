99701ab02d030ce7ccc90a20f42aed614bf47447
xestho <raphael.suter@stud.hslu.ch>
2014-05-06 11:40:46 +0200

    Testclass

--- ConnectFour/src/connectfour/ConnectFour.java
+++ ConnectFour/src/connectfour/ConnectFour.java
@@ -18,6 +18,7 @@ public class ConnectFour {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("blabla");
+        System.out.print("blabla222");
     }
     
 }
