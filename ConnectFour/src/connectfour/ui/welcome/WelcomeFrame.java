15decae7a0a5000b7d06c867061f9fdafc64a793
Alex <alexander.suter@gmx.ch>
2014-05-10 14:29:58 +0200

    - MVC GameUI implementiert - Utility Class für UI implementiert

--- ConnectFour/src/connectfour/ui/welcome/WelcomeFrame.java
+++ ConnectFour/src/connectfour/ui/welcome/WelcomeFrame.java
@@ -1,89 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package connectfour.ui.welcome;
 
-import connectfour.ui.game.GameFrame;
+import connectfour.ui.game.GameController;
+import connectfour.ui.game.GameView;
+import connectfour.ui.util.CenterWindowUtil;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
+ * Welcome Frame stellt das erste Frame in der Applikation dar. In diesem GUI
+ * hat der User die Auswahl ob er gegen den Computer oder gegen einen Mensch
+ * spielen will.
  *
  * @author Alex
  */
+
+// TODO Implementierung von MVC durch Java Beginners :)
 public class WelcomeFrame {
-    
+
     private static final String FRAME_TITLE = "Connect 4 - Welcome";
-    
-    private static final String BUTTON_SINGLE_PLAYER = "Einzelspieler";
-    
-    private static final String BUTTON_MULTI_PLAYER = "Mehrspieler";
-    
+
+    private static final String BUTTON_SINGLE_PLAYER = "Computer";
+
+    private static final String BUTTON_MULTI_PLAYER = "Mensch";
+
     private JFrame frame;
-    
+
     private JButton singlePlayerButton;
-    
+
     private JButton multiPlayerButton;
-    
+
     public WelcomeFrame() {
         createFrame();
-       
+
         createSinglePlayerButton();
         createMultiPlayerButton();
 
-        // setGridSize(); oder so Ähnlich
- 
         createLayout();
         setupLayout();
     }
-    
+
     public void show() {
         frame.setVisible(true);
     }
-    
+
     private void createFrame() {
         frame = new JFrame(FRAME_TITLE);
         frame.setSize(300, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        CenterWindowUtil.center(frame);
+        frame.setResizable(false);
     }
-    
+
     private void createLayout() {
-        //LayoutManager layoutManager = new GridLayout(2, 1);
-        LayoutManager layoutManager = new BorderLayout(10, 10);
-        frame.setLayout(layoutManager);
+        frame.setLayout(null);
     }
-    
+
     private void setupLayout() {
-        JPanel panel = new JPanel(new FlowLayout());
-        panel.add(singlePlayerButton);
-        panel.add(multiPlayerButton);
-        frame.add(panel, BorderLayout.CENTER);
+        singlePlayerButton.setBounds(75, 50, 150, 30);
+        multiPlayerButton.setBounds(75, 100, 150, 30);
+        frame.getContentPane().add(singlePlayerButton);
+        frame.getContentPane().add(multiPlayerButton);
     }
-    
+
     private void createSinglePlayerButton() {
         singlePlayerButton = new JButton(BUTTON_SINGLE_PLAYER);
         singlePlayerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Destroy this window and create the game window
-                System.out.println("Clicked on single player");
-                // TODO Zerstört das wirklich das objekt, denke nicht?
                 frame.setVisible(false);
-                
-                GameFrame gameFrame = new GameFrame();
-                gameFrame.show();
+                frame.dispose();
+
+                GameController controller = new GameController();
+                controller.showView();
             }
         });
     }
-    
+
     private void createMultiPlayerButton() {
-         multiPlayerButton = new JButton(BUTTON_MULTI_PLAYER);
+        multiPlayerButton = new JButton(BUTTON_MULTI_PLAYER);
         multiPlayerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -91,5 +87,5 @@ public class WelcomeFrame {
             }
         });
     }
-    
+
 }
