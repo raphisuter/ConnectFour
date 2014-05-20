48bdfd9e4c2a3ab6754f9d0baa7b230434244967
xestho <raphael.suter@stud.hslu.ch>
2014-05-20 17:31:50 +0200

    Neuste Testversion vom Connector

--- ConnectFour/src/connectfour/networking/TCPCommunicator.java
+++ ConnectFour/src/connectfour/networking/TCPCommunicator.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -36,16 +37,30 @@ public class TCPCommunicator {
     }
     
     public void sendThrow(int column){
-        try(Socket client = new Socket(opponentAddr, NetworkHelper.Port)){
+        /*try(Socket client = new Socket(opponentAddr, NetworkHelper.Port)){
             PrintWriter outStream = new PrintWriter(client.getOutputStream());
             outStream.println(column);
             outStream.flush();
         }catch(IOException e){
             e.printStackTrace();
+        }*/
+        
+        try (Socket sock = new Socket(opponentAddr, NetworkHelper.Port)){
+            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+            //Output
+            out.println(column);
+            out.flush();
+
+            out.close();
+            sock.close();
+        } catch(Exception e) {
+            e.printStackTrace();
         }
+        
     }
     
     public int receiveThrow(){
+        /*
         try(Socket client = new Socket(opponentAddr, NetworkHelper.Port)){
             BufferedReader inStream = new BufferedReader(new InputStreamReader(client.getInputStream()));
             String line;
@@ -56,6 +71,25 @@ public class TCPCommunicator {
             e.printStackTrace();
         }
         return 0;
+        */
+        
+        try (ServerSocket serverSock = new ServerSocket(NetworkHelper.Port)){
+            //Get connection
+            Socket clientSock = serverSock.accept();
+            System.out.println("Connected client");
+
+            //Get input
+            BufferedReader br = new BufferedReader(new InputStreamReader(clientSock.getInputStream()));
+            int ret = (Integer.parseInt(br.readLine()));
+
+            br.close();
+            serverSock.close();
+            clientSock.close();
+            return ret;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 
     public InetAddress getOpponentAddr(){
