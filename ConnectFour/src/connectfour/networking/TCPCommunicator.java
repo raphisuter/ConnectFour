4b2504617cc51acc4a4bf4edf1f66168fa2bf7c1
xestho <raphael.suter@stud.hslu.ch>
2014-05-20 17:36:30 +0200

    Bugfixes und überflüssige Imports entfernt

--- ConnectFour/src/connectfour/networking/TCPCommunicator.java
+++ ConnectFour/src/connectfour/networking/TCPCommunicator.java
@@ -7,7 +7,6 @@
 package connectfour.networking;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
