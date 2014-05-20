e6ac02488a93eabc0558bcf9f73045a4119bbb85
Alex <alexander.suter@gmx.ch>
2014-05-20 15:27:52 +0200

    - Diverse Verbesserungen

--- ConnectFour/src/connectfour/ui/searchServer/SearchServerController.java
+++ ConnectFour/src/connectfour/ui/searchServer/SearchServerController.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package connectfour.ui.searchServer;
 
-import connectfour.model.NetworkPlayer;
+import connectfour.model.DefaultConfiguration;
 import connectfour.model.Player;
 import connectfour.networking.UDPClient;
 import connectfour.networking.UDPPlay;
 import connectfour.ui.game.GameController;
 import connectfour.ui.welcome.WelcomeController;
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 
 /**
  *
@@ -25,46 +17,44 @@ public class SearchServerController {
 
     private SearchServerModel model;
     private SearchServerView view;
-    
+
     private UDPClient client;
 
     public SearchServerController(UDPClient setClient) {
         this.model = new SearchServerModel();
+        
+        // Wir warten 2 Sekunden bis auf dem Client die IP Adress Liste gestzt ist.
         try {
-        Thread.sleep(2000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        
+
         this.view = new SearchServerView(setClient.getClientAddressList().toArray());
         this.client = setClient;
 
         this.view.addActionListenerActualButton(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                System.out.println("Aktualisieren wurde gedrückt"); //Test-Statement
-                
+                System.out.println("Aktualisieren der IP Adressen");
                 view.setIPadresses(client.getClientAddressList().toArray());
             }
 
         });
-        
+
         this.view.addActionConnectButton(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                System.out.println("Verbinden wurde gedrückt"); //Test-Statement
-                
                 String ip = view.getSelectedIPaddress();
-                
-                //Antwort an den Server mit der IP, so dass die sich verbinden können
+                System.out.println("Verbinden mit " + ip);
+
                 UDPPlay uplay = new UDPPlay(ip);
                 uplay.start();
                 
-                NetworkPlayer networkPlayer = new NetworkPlayer(2, "Netzwerkspieler " + ip , Color.GREEN, ip);
-                
-                GameController controller = new GameController(networkPlayer, Player.createLocalPlayer(), 7, 6);
+                Player startPlayer = Player.createNetworkPlayer(ip);
+                Player otherPlayer = Player.createLocalPlayer();
+
+                GameController controller = new GameController(startPlayer, otherPlayer, DefaultConfiguration.COLUMNS, DefaultConfiguration.ROWS);
                 controller.showView();
             }
 
@@ -73,36 +63,25 @@ public class SearchServerController {
         this.view.addActionListenerBackButton(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                System.out.println("Zurück");
 
-                System.out.println("Zurück wurde gedrückt"); //Test-Statement
-                
+                // Fenster schliessen
                 view.close();
-                
-                //UDPClient stoppen
+
+                // UDPClient stoppen
                 client.setStopThread(true);
-                
+
+                // Welcome zeigen
                 WelcomeController controller = new WelcomeController();
                 controller.showView();
-                
             }
 
         });
-
-        this.view.addListSelectionListener(new ListSelectionListener() {
-
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-                System.out.println("Auswahl in Liste wurde angewählt" + e.toString()); //Test-Statement
-                //selectedIPAdress = e.
-            }
-
-        });
-
+        
     }
 
     public void showView() {
         this.view.show();
-
     }
 
 }
